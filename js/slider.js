69a03cdf83a51c09730963365d5c5607a3520a45
emon-webdev <emon.webdev@gmail.com>
2022-09-16 10:15:38 +0600

    set slider

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,24 @@
-setInterval()
+const images = [
+    'images/photo1.jpg',
+    'images/photo2.jpg',
+    'images/photo3.jpg',
+    'images/photo4.jpg',
+    'images/photo5.jpg',
+];
+
+let imgIndex = 0;
+
+const img = document.getElementById('carousel-img');
+
+setInterval(() => {
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    const imgLink = images[imgIndex];
+    img.setAttribute('src', imgLink)
+    console.log(imgLink)
+    imgIndex++
+}, 1000)
 
 
 
